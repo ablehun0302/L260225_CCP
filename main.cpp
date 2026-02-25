1545b38dad557c92fafc277f288bc72071801e97
Kim Younghun <ablehun@naver.com>
2026-02-25 09:06:16 +0900

    초기 파일
    정수 두 개 받아서 연산하기

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int Number_1 = 0;
+	int Number_2 = 0;
+	cin >> Number_1;
+	cin >> Number_2;
+
+	cout << "plus: " << Number_1 + Number_2 << endl;
+	cout << "minus: " << Number_1 - Number_2 << endl;
+	cout << "multiply: " << Number_1 * Number_2 << endl;
+	cout << "divide: " << Number_1 / Number_2 << endl;
+	cout << "modulo: " << Number_1 % Number_2 << endl;
+}
